03a0e7ee31891423e02d7c1e7fd8571342f1b89c
mwaltr <walter.eidi@interfocus.com.br>
2021-10-07 14:00:36 -0300

    projetoestagio
    t

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        { 
+           int texto = 11;
+
+            return View(texto);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Conteudo()
+        {
+            ModelDep modeldep = new ModelDep();
+
+            List<ControllerTeste> list = new List<ControllerTeste>();
+            list.Add(new ControllerTeste { Id = 1, Name = "elet" });
+            list.Add(new ControllerTeste { Id = 2, Name = "elestroma" });
+
+            var r2 = list.Where( p => p.Name == "elet").Select(p => p.Name);
+
+            ViewData["Title"] = modeldep.ModDep();
+            ViewData["List"] = r2;
+
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication2/Models/ModelDep.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WebApplication2.Models
+{
+    public class ModelDep
+    {
+        
+
+        private string v;
+
+        public ModelDep()
+        {
+        }
+
+        public ModelDep(string v)
+        {
+            this.v = v;
+        }
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public  String ModDep()
+        {
+          
+            return "testzze";
+          
+
+        }
+
+    }
+}
